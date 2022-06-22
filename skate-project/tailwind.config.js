735e48ff92b3f57822c001bc9e98db1178180334
Daniel <daniel.studders@rs-components.com>
2022-06-22 09:44:27 +0100

    SK-1 Initialising Project

---
+++ skate-project/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jax,tsx}'
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
